77ebe1cfbda4beeb34c0d9fdc182ed1bfbec029a
EdilbertoHaintech <edilberto.sanchez@haintech.cl>
2023-03-10 14:50:59 -0500

    Added control header on Medicines section, without styles

---
+++ src/components/Action Side Bar/Control Header Table.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="control_header d-flex gap-10 justify-space-between align-center flex-wrap text-unselectable">
+        <div class="search">
+            <button type="button" class="icon">
+                <label for="search">
+                    <img src="assets/Search.svg" alt="Buscador" class="mr-2 white_filter">
+                </label>
+            </button>
+            <input type="text" id="search" placeholder="Buscar">
+        </div>
+        <div class="actions-section d-flex gap-10">
+            <button type="button" class="icon">
+                <img src="assets/Add.svg" alt="Agregar" class="mr-2 white_filter">
+            </button>
+            <button type="button" class="icon">
+                <img src="assets/Edit.svg" alt="Editar" class="mr-2 white_filter">
+            </button>
+            <button type="button" class="icon">
+                <img src="assets/Delete.svg" alt="Eliminar" class="mr-2 white_filter">
+            </button>
+        </div>
+    </div>
+</template>
+
+<style>
+.control_header {
+    background-color: var(--primary-color);
+}
+</style>
--- src/components/Action Side Bar/Medicines.vue
+++ src/components/Action Side Bar/Medicines.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="d-flex flex-column">
         <span class="title_section">Medicamentos</span>
-        
+        <ControlHeaderTable></ControlHeaderTable>
     </div>
 </template>
+
+<script lang="ts">
+import ControlHeaderTable from './Control Header Table.vue';
+
+export default {
+    components: {
+        ControlHeaderTable
+    },
+};
+</script>
