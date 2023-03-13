0901274e7e28bb19a43fc78f0bf9f43e7a0ff8ea
EdilbertoHaintech <edilberto.sanchez@haintech.cl>
2023-03-12 22:33:27 -0500

    Showing data from JSON file simple example on Medicines, It's a practice, no real code

--- src/components/Action Side Bar/Medicines.vue
+++ src/components/Action Side Bar/Medicines.vue
@@ -3,7 +3,7 @@
         <span class="title_section">Medicamentos</span>
         <ControlHeaderTable></ControlHeaderTable>
         <!-- Generic Table -->
-        <table>
+        <!-- <table>
             <thead>
                 <tr>
                     <th>Reembolso</th>
@@ -32,17 +32,44 @@
                     <td>$15.00</td>
                 </tr>
             </tbody>
-        </table>
+        </table> -->
 
+        <ul>
+            <li v-for="user in data1.usuarios" :key="user.id">
+                {{ user.nombre }} {{ user.apellido }}
+            </li>
+        </ul>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in data2" :key="user.id">
+                    <td>{{ user.nombre }}</td>
+                    <td>{{ user.apellido }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script lang="ts">
 import ControlHeaderTable from './Control Header Table.vue';
+import SimpleExample from "../../../public/JSON/Simple Example";
+import SimpleExample1 from "../../../public/JSON/Simple Example - Copy";
 
 export default {
     components: {
         ControlHeaderTable
     },
+    data() {
+        return {
+            data1: SimpleExample,
+            data2: SimpleExample1
+        }
+    }
 };
 </script>
